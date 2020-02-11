62f763e38fbc9a5da052a3fbeb1b75ac554b4ecd
Mayur Kale <mayurkale@google.com>
2020-02-10 22:25:50 -0800

    format code

--- src/main/java/com/example/spanner/PersonController.java
+++ src/main/java/com/example/spanner/PersonController.java
@@ -22,55 +22,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/spanner")
 public class PersonController {
 
-	private Spanner spanner;
-	private DatabaseClient dbClient;
-	String instanceId = "demo";
-	String databaseId = "hello_spanner";
-	String table = "Players";
+  private Spanner spanner;
+  private DatabaseClient dbClient;
+  String instanceId = "demo";
+  String databaseId = "hello_spanner";
+  String table = "Players";
 
-	PersonController() {
-		// Instantiate the client.
-		SpannerOptions options = SpannerOptions.getDefaultInstance();
-		spanner = options.getService();
-		// And then create the Spanner database client.
-		String projectId = options.getProjectId();
-		dbClient = spanner.getDatabaseClient(DatabaseId.of(projectId, instanceId, databaseId));
+  PersonController() {
+    // Instantiate the client.
+    SpannerOptions options = SpannerOptions.getDefaultInstance();
+    spanner = options.getService();
+    // And then create the Spanner database client.
+    String projectId = options.getProjectId();
+    dbClient = spanner.getDatabaseClient(DatabaseId.of(projectId, instanceId, databaseId));
 
-		//BasicSetup.enableOpenCensus();
-	}
+    // BasicSetup.enableOpenCensus();
+  }
 
-	@GetMapping(path="/", produces = "application/json")
-	public List<Person> getPerson() {
-		List<Person> list = new ArrayList<>();
-		try (ResultSet resultSet = dbClient
-			.singleUse()
-			.read(
-				table,
-				KeySet.all(), // Read all rows in a table.
-				Arrays.asList("first_name", "last_name", "uuid"))) {
-			while (resultSet.next()) {
-				Person person = new Person();
-				person.setId(resultSet.getString(0));
-				person.setName(resultSet.getString(1));
-				person.setEmail(resultSet.getString(2));
-				list.add(person);
-			}
-		}
-		return list;
-	}
+  @GetMapping(path = "/", produces = "application/json")
+  public List<Person> getPerson() {
+    List<Person> list = new ArrayList<>();
+    try (ResultSet resultSet =
+        dbClient
+            .singleUse()
+            .read(
+                table,
+                KeySet.all(), // Read all rows in a table.
+                Arrays.asList("first_name", "last_name", "uuid"))) {
+      while (resultSet.next()) {
+        Person person = new Person();
+        person.setId(resultSet.getString(0));
+        person.setName(resultSet.getString(1));
+        person.setEmail(resultSet.getString(2));
+        list.add(person);
+      }
+    }
+    return list;
+  }
 
-	@PostMapping(path= "/", consumes = "application/json", produces = "application/json")
-	public Person addPerson(@RequestBody Person p) {
-		List<Mutation> mutations =
-			Collections.singletonList(Mutation.newInsertBuilder(table)
-				.set("id")
-				.to(UUID.randomUUID().toString())
-				.set("name")
-				.to(p.getName())
-				.set("email")
-				.to(p.getEmail())
-				.build());
-		dbClient.write(mutations);
-		return p;
-	}
+  @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+  public Person addPerson(@RequestBody Person p) {
+    List<Mutation> mutations =
+        Collections.singletonList(
+            Mutation.newInsertBuilder(table)
+                .set("id")
+                .to(UUID.randomUUID().toString())
+                .set("name")
+                .to(p.getName())
+                .set("email")
+                .to(p.getEmail())
+                .build());
+    dbClient.write(mutations);
+    return p;
+  }
 }
